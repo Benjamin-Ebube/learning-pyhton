f065575530124b1726a4b8f4d2c990a1b93767af
Benjamin-Ebube <benjyebube@gmail.com>
2024-07-17 10:07:34 +0100

    learnt about lists and dictionaries.

--- learning/learning.py
+++ learning/learning.py
@@ -54,22 +54,70 @@
 
 #boutta build a car game ya'll
 
-help = "start - to start the car\nstop - to stop the car\nquit - to exit the game"
-start = 'your car has started!'
-stop = 'your car has stopped'
-print('welcome')
-attempts = 0
-trials = 10
-while attempts < trials:
-    response = input('> ')
-    attempts = attempts + 1
-    if response == 'help':
-       print(help)
-    elif response == 'start':
-        print(start)
-    elif response == 'stop':
-        print(stop)
-    elif response == 'quit':
-        exit()
-    else:
-     print('Sorry i dont understand that. Try "help" for the instruction manual.')
+#help = "start - to start the car\nstop - to stop the car\nquit - to exit the game"
+#start = 'your car has started!'
+#stop = 'your car has stopped'
+#print('welcome')
+#while True:
+#    response = input('> ').lower()
+#    if response == 'help':
+#       print(help)
+#    elif response == 'start':
+#        print(start)
+#    elif response == 'stop':
+#        print(stop)
+#   elif response == 'quit':
+#        exit()
+#    else:
+#     print('Sorry i dont understand that. Try "help" for the instruction manual.')
+
+
+#learning "for" loops
+#prices = [10, 20, 30]
+# to calculate the total of the amount of properties in your shopping cart
+#total = 0
+#for price in prices:
+#    total = total + price
+#    print(f"Total: {total}")
+
+
+# this is to remove duplicates
+#numbers = [4, 6, 7, 2, 4, 9, 5, 0, 0]
+#unique = []
+#for values in numbers:
+#    if values not in unique:
+#        unique.append(values)
+#print(unique)
+
+# back to 'tuples' again. For tuples they are immutable('cant be changed')
+#tuple_example = (1, 2, 4, 'howard', 9 )
+#print(tuple_example)
+# tuples are like lists just that they cant be modified.
+
+# boutta show an example of 'unpacking' using tuples
+#coordinates = (2, 6, 9, 'laps')
+# this is a better way of assigning each item to a variables.
+#a, b, c, d = coordinates
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+# it also works on lists too.
+
+
+# learning about dictionaries now. THEY make use of key values.
+#customer = {
+#    'name': 'benjamin',
+#    'age': 16,
+#    'innocent': True
+#}
+#print(customer['name'])
+# you can also use the get function to do it also
+#print(customer.get('age'))
+
+
+
+
+
+
+
